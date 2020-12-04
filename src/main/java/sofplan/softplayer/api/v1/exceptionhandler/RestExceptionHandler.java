e8d09635f13b82fc75f597e2fb66589cc91721ac
Diego <diego@bussola.com.br>
2020-12-04 20:20:12 -0300

    Configuração inicial do spring security

--- src/main/java/sofplan/softplayer/api/v1/exceptionhandler/RestExceptionHandler.java
+++ src/main/java/sofplan/softplayer/api/v1/exceptionhandler/RestExceptionHandler.java
@@ -1,16 +1,25 @@
 package sofplan.softplayer.api.v1.exceptionhandler;
 
+import com.sun.istack.Nullable;
 import javassist.NotFoundException;
 import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
+import sofplan.softplayer.domain.exception.ExceptionDetails;
 import sofplan.softplayer.domain.exception.PeopleNotFoundException;
 import sofplan.softplayer.domain.exception.PeopleNotFoundExceptionDetails;
+import sofplan.softplayer.domain.exception.ValidationExceptionDetails;
 
 import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @ControllerAdvice
 @Log4j2
@@ -25,5 +34,40 @@ public class RestExceptionHandler extends ResponseEntityExceptionHandler {
                         .details(nfe.getMessage())
                         .developerMessage(nfe.getClass().getName())
                         .build(), HttpStatus.NOT_FOUND);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleMethodArgumentNotValid(
+            MethodArgumentNotValidException exception, HttpHeaders headers, HttpStatus status, WebRequest request) {
+        List<FieldError> fieldErrors = exception.getBindingResult().getFieldErrors();
+
+        String fields = fieldErrors.stream().map(FieldError::getField).collect(Collectors.joining(", "));
+        String fieldsMessage = fieldErrors.stream().map(FieldError::getDefaultMessage).collect(Collectors.joining(", "));
+
+        return new ResponseEntity<>(
+                ValidationExceptionDetails.builder()
+                        .timestamp(LocalDateTime.now())
+                        .status(HttpStatus.BAD_REQUEST.value())
+                        .title("Bad Request Exception, Invalid Fields")
+                        .details("Check the field(s) error")
+                        .developerMessage(exception.getClass().getName())
+                        .fields(fields)
+                        .fieldsMessage(fieldsMessage)
+                        .build(), HttpStatus.BAD_REQUEST);
+    }
+
+    @Override
+    protected ResponseEntity<Object> handleExceptionInternal(
+            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatus status, WebRequest request) {
+
+        ExceptionDetails exceptionDetails = ExceptionDetails.builder()
+                .timestamp(LocalDateTime.now())
+                .status(status.value())
+                .title(ex.getCause().getMessage())
+                .details(ex.getMessage())
+                .developerMessage(ex.getClass().getName())
+                .build();
+
+        return new ResponseEntity<>(exceptionDetails, headers, status);
     }
 }
