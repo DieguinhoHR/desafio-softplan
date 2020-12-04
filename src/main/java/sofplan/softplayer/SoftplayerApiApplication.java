5fd04543ecac6c72875ebdef7f2d9415e94b7cbc
Diego <diego@bussola.com.br>
2020-12-03 21:25:05 -0300

    Add docker ao projeto

---
+++ src/main/java/sofplan/softplayer/SoftplayerApiApplication.java
@@ -0,0 +1,13 @@
+package sofplan.softplayer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SoftplayerApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SoftplayerApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/sofplan/softplayer/SoftplayerApiApplicationTests.java
@@ -0,0 +1,13 @@
+package sofplan.softplayer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SoftplayerApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
