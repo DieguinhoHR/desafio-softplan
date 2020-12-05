54ffc5731adea12c3ee57380aeb94cbe4fa7ac8d
Diego <diego@bussola.com.br>
2020-12-05 12:35:07 -0300

    Movido arquivos para pasta backend

---
+++ api/target/generated-sources/annotations/sofplan/softplayer/domain/model/mapper/PeopleMapperImpl.java
@@ -0,0 +1,38 @@
+package sofplan.softplayer.domain.model.mapper;
+
+import javax.annotation.processing.Generated;
+import org.springframework.stereotype.Component;
+import sofplan.softplayer.api.v1.dto.PeopleDTO;
+import sofplan.softplayer.api.v1.dto.PeopleNewDTO;
+import sofplan.softplayer.domain.model.People;
+
+@Generated(
+    value = "org.mapstruct.ap.MappingProcessor",
+    date = "2020-12-05T10:31:33-0300",
+    comments = "version: 1.4.1.Final, compiler: javac, environment: Java 14.0.1 (Oracle Corporation)"
+)
+@Component
+public class PeopleMapperImpl implements PeopleMapper {
+
+    @Override
+    public People toPeople(PeopleNewDTO peoplePostRequestBody) {
+        if ( peoplePostRequestBody == null ) {
+            return null;
+        }
+
+        People people = new People();
+
+        return people;
+    }
+
+    @Override
+    public People toPeople(PeopleDTO peopleDTO) {
+        if ( peopleDTO == null ) {
+            return null;
+        }
+
+        People people = new People();
+
+        return people;
+    }
+}
