07cf4eab4c4a6442d7ef5a6197e0e6773a3a8fb5
Diego <diego@bussola.com.br>
2020-12-08 00:45:50 -0300

    Add masks com vanilla js

--- frontend/src/components/people/create.js
+++ frontend/src/components/people/create.js
@@ -4,10 +4,10 @@ import {
   Row,
   Col,
   Form,
+  Card,
 } from 'react-bootstrap'
-import 
-  Button
- from 'react-bootstrap/Button';
+import Button from 'react-bootstrap/Button';
+import { cpfMask, dateOfBirthMask } from '../../utils/masks';
 
 function Create() {
   const [values, setValues] = useState({    
@@ -20,12 +20,8 @@ function Create() {
     cpf: ''
   });
 
-  // const handleSave = (values) => {
-  //   api.post('/login', values)
-
-    
-  // }
   async function handleSave(values) {
+    console.log(values.gender)
     const obj = await api.post('/v1/admin/people', {
       'date_birth': values.dateBirth,
       ...values
@@ -42,95 +38,110 @@ function Create() {
   };
 
   return (
-    <Form onSubmit={event => {
-        event.preventDefault();
-        handleSave(values);
-    }}>
-      <Row>
-        <Col xs="6">
-          <Form.Label>Nome</Form.Label>
-          <Form.Control type="name" 
-            className="text-muted"
-            name="name"
-            value={values.name}
-            onChange={handleChange}
-            required
-            placeholder="Digite seu nome de usuário" />         
-        </Col>
-        <Col xs="6">
-          <Form.Label>Email</Form.Label>          
-          <Form.Control type="email" 
-            className="text-muted"
-            name="email"
-            value={values.email}
-            onChange={handleChange}
-            required
-            placeholder="Digite seu email" />      
-        </Col>        
-      </Row>
+    <div className="container">
+      <Card>
+        <Card.Header as="h5">Cadastro de pessoa</Card.Header>
+        <Card.Body>
+          <Form onSubmit={event => {
+              event.preventDefault();
+              handleSave(values);
+          }}>
+            <Form.Group>
+              <Row>
+                <Col xs="6">
+                  <Form.Label>Nome</Form.Label>
+                  <Form.Control type="name" 
+                    className="text-muted"
+                    name="name"
+                    value={values.name}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite seu nome de usuário" />         
+                </Col>
+                <Col xs="6">
+                  <Form.Label>Email</Form.Label>          
+                  <Form.Control type="email" 
+                    className="text-muted"
+                    name="email"
+                    value={values.email}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite seu email" />      
+                </Col>        
+              </Row>
+            </Form.Group>
 
-      <Row>
-        <Col xs="6">
-          <Form.Label>Data de nascimento</Form.Label>
-          <Form.Control type="dateBirth" 
-            className="text-muted"
-            name="dateBirth"
-            value={values.dateBirth}
-            onChange={handleChange}
-            required
-            placeholder="Digite sua data de nascimento" />      
-        </Col>
-        <Col xs="6">
-          <Form.Label>Sexo</Form.Label>
-          <Form.Control type="gender" 
-            className="text-muted"
-            name="gender"
-            value={values.gender}
-            onChange={handleChange}
-            required
-            placeholder="Digite sua data de nascimento" />  
-        </Col>
-      </Row>
+            <Form.Group >
+              <Row>
+                <Col xs="6">
+                  <Form.Label>Data de nascimento</Form.Label>
+                  <Form.Control type="dateBirth" 
+                    className="text-muted"
+                    name="dateBirth"
+                    value={dateOfBirthMask(values.dateBirth)}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite sua data de nascimento" />      
+                </Col>
+                <Col xs="6">
+                  <Form.Label>Sexo</Form.Label>                  
+                    <Form.Control 
+                      as="select" 
+                      name="gender"
+                      id="gender"
+                      defaultValue="Sexo"
+                      onChange={handleChange}
+                    >
+                      <option value="0">Masculino</option>
+                      <option value="1">Feminino</option>
+                    </Form.Control>
+                </Col>
+              </Row>
+            </Form.Group>
 
-      <Row>        
-        <Col xs="4">
-          <Form.Label>Natural</Form.Label>
-          <Form.Control type="naturalness" 
-            className="text-muted"
-            name="naturalness"
-            value={values.naturalness}
-            onChange={handleChange}
-            required
-            placeholder="Digite sua naturalidade" />  
-        </Col>
-      
-        <Col xs="4">
-          <Form.Label>Nacionalidade</Form.Label>
-          <Form.Control type="nationality" 
-            className="text-muted"
-            name="nationality"
-            value={values.nationality}
-            onChange={handleChange}
-            required
-            placeholder="Digite sua nacionalidade" />  
-        </Col>
+            <Form.Group> 
+              <Row>   
+                <Col xs="3">
+                  <Form.Label>CPF</Form.Label>
+                  <Form.Control type="cpf" 
+                    className="text-muted"
+                    name="cpf"
+                    value={cpfMask(values.cpf)}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite seu CPF" />  
+                </Col>     
+              
+                <Col xs="3">
+                  <Form.Label>Nacionalidade</Form.Label>
+                  <Form.Control type="nationality" 
+                    className="text-muted"
+                    name="nationality"
+                    value={values.nationality}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite sua nacionalidade" />  
+                </Col>
 
-        <Col xs="4">
-          <Form.Label>CPF</Form.Label>
-          <Form.Control type="cpf" 
-            className="text-muted"
-            name="cpf"
-            value={values.cpf}
-            onChange={handleChange}
-            required
-            placeholder="Digite seu CPF" />  
-        </Col>
-      </Row>
-      <br />
-      <Button variant="primary" type="submit">
-        Salvar
-      </Button>
-    </Form>  
+                <Col xs="6">                  
+                  <Form.Label>Natural</Form.Label>
+                  <Form.Control type="naturalness" 
+                    className="text-muted"
+                    name="naturalness"
+                    value={values.naturalness}
+                    onChange={handleChange}
+                    required
+                    placeholder="Digite sua naturalidade" />  
+                </Col>
+              </Row>
+            </Form.Group>  
+            <Button variant="primary" type="submit">
+              Salvar
+            </Button>
+          </Form> 
+        </Card.Body>
+      </Card>
+    </div> 
   )
 }
 
---
+++ frontend/src/utils/masks.js
@@ -0,0 +1,21 @@
+function cpfMask (value) {
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d)/, '$1.$2')
+        .replace(/(\d{3})(\d{1,2})/, '$1-$2')
+        .replace(/(-\d{2})\d+?$/, '$1')
+}
+
+function dateOfBirthMask (value) {
+    return value
+        .replace(/\D/g, '')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{2})(\d)/, '$1/$2')
+        .replace(/(\d{4})(\d)/, '$1')
+}
+
+export { 
+    cpfMask,
+    dateOfBirthMask
+}
